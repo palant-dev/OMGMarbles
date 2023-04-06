cfd9830babb67c59a522a6f793f457f19e6bc876
palant-dev <113528707+apalomba22@users.noreply.github.com>
2023-04-06 17:41:28 +0200

    Set-up the grid of balls and added physical properties

--- OMGMarbles/GameScene.swift
+++ OMGMarbles/GameScene.swift
@@ -20,6 +20,33 @@ class GameScene: SKScene {
         background.alpha = 0.2
         background.zPosition = -1
         addChild(background)
+
+        // Here we are creating the ball element with its radius
+        let ball = SKSpriteNode(imageNamed: "ballBlue")
+        let ballRadius = ball.frame.width / 2.0
+
+        // This is a for loop for creating a matrix in which display the balls
+        for i in stride(from: ballRadius, to: view.bounds.width - ballRadius, by: ball.frame.width) {
+            for j in stride(from: 100, to: view.bounds.height - ballRadius, by: ball.frame.height) {
+                // We add the force unwrap here to avoid and error at let ball = Ball(imageNamed: ballType)
+                let ballType = balls.randomElement()!
+                let ball = Ball(imageNamed: ballType)
+                ball.position = CGPoint(x: i, y: j)
+                ball.name = ballType
+                addChild(ball)
+
+                /// Adding physic property to the balls:
+                ///     - circleOfRadius: set the dimensions of the collision box
+                ///     - restitution: set the bounciness of the balls
+                ///     - friction: how smooth the contatct between the balls should be
+                ball.physicsBody = SKPhysicsBody(circleOfRadius: ballRadius)
+                ball.physicsBody?.allowsRotation = false
+                ball.physicsBody?.restitution = 0
+                ball.physicsBody?.friction = 0
+            }
+        }
+
+
     }
 
     override func update(_ currentTime: TimeInterval) {
